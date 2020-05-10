dc87c77b18795bce58e04d08e179d3bfdc9bcbad
Lopson <balzhinimaev-2011@mail.ru>
2020-05-10 20:26:56 +0900

    Created component: Tiles.tsx - container component to handle connection with store

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, {FC} from 'react';
+import styles from './Tiles.module.css'
 
-const Tiles = () => {
+// Chile components:
+import {TileElement} from "./TileElement";
+
+
+import {TILE_TYPE} from "../store/TilesReducer/types";
+import {AppActions, AppState} from '../store';
+import {connect} from "react-redux";
+import {Dispatch} from "redux";
+import {handleClickOnTile, startGame, checkTiles} from "../store/TilesReducer/actions";
+
+interface TilesProps {
+    tiles: Array<TILE_TYPE>;
+    startGame: () => void;
+    clickOnTile: (tile: TILE_TYPE) => void;
+    checkTiles: () => void;
+}
+
+const DumbComponent: FC<TilesProps> = ({tiles,startGame, clickOnTile, checkTiles}) => {
+    
+    const tileElements = tiles.map(tile => {
+        return (
+            <TileElement key={tile.id}
+                         tile={tile}
+                         clickOnTile={clickOnTile}
+                         checkTiles={checkTiles}
+            />
+        )
+    })
+    
+    const handleStartGame = () => {
+        startGame();
+    }
+    
     return (
-        <div>
-	       
+        <div className={styles.tiles__container}>
+            {tileElements}
+            <button onClick={handleStartGame}>Start game</button>
         </div>
     )
 }
 
-export {Tiles};
+const mapStateToProps = (state: AppState) => {
+    return {
+        tiles: state.TilesReducer.tiles,
+    }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch<AppActions>) => {
+    return {
+        startGame: () => dispatch(startGame()),
+        clickOnTile: (tile: TILE_TYPE) => dispatch(handleClickOnTile(tile)),
+        checkTiles: () => dispatch(checkTiles()),
+    }
+}
+export const Tiles = connect(mapStateToProps, mapDispatchToProps)(DumbComponent);
