196b03c4f77244446d84337820d500ef7534d4c7
Lopson <balzhinimaev-2011@mail.ru>
2020-05-10 20:23:33 +0900

    Created root reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,10 @@
-export {}
+import {combineReducers} from "redux";
+import {TilesReducer} from "./TilesReducer/tiles-reducer";
+import {TILES_ACTION_TYPES} from "./TilesReducer/types";
+
+export const rootReducer = combineReducers({
+	TilesReducer
+})
+
+export type AppState = ReturnType<typeof rootReducer>
+export type AppActions = TILES_ACTION_TYPES
