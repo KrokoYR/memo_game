5017eef1e68e9dd327bf618fdb55ebede912416b
Lopson <balzhinimaev-2011@mail.ru>
2020-05-10 20:27:18 +0900

    Dumb component to display single tile behavior

--- src/components/TileElement.tsx
+++ src/components/TileElement.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {FC, useEffect} from 'react';
+import styles from './TileElement.module.css'
 
-const TileElement = () => {
-    return (
-        <div>
-	       
-        </div>
-    )
+import {TILE_TYPE} from '../store/TilesReducer/types';
+
+interface TileElementProps {
+	tile: TILE_TYPE,
+	clickOnTile: (tile: TILE_TYPE) => void,
+	checkTiles: () => void,
+}
+
+const TileElement: FC<TileElementProps> = ({tile, clickOnTile, checkTiles}) => {
+	
+	useEffect(() => {
+		setTimeout(() => {checkTiles()}, 500);
+	})
+	
+	const handleOnClick = () => {
+		console.log(tile.id, 'Clicked', tile.backColor);
+		clickOnTile(tile);
+		//setTimeout(() => {checkTiles()}, 500);
+	}
+	
+	return (
+		<div onClick={handleOnClick} className={styles.tiles__element} style={{backgroundColor: tile.frontColor}}/>
+	)
 }
 
 export {TileElement};
