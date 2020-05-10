3f54685ef4541a9f3072c0bbe557ab7b8a67bdaf
Lopson <balzhinimaev-2011@mail.ru>
2020-05-10 19:08:40 +0900

    Types for tiles-reducer.ts

--- src/store/TilesReducer/actions.ts
+++ src/store/TilesReducer/actions.ts
@@ -1,1 +1,26 @@
-export {}
+import {CHECK_TILES, HANDLE_CLICK_ON_TILE, RESTART_GAME, START_GAME, TILE_TYPE, TILES_ACTION_TYPES} from "./types";
+
+export const handleClickOnTile = (tile: TILE_TYPE): TILES_ACTION_TYPES => {
+	return {
+		type: HANDLE_CLICK_ON_TILE,
+		tile,
+	}
+}
+
+export const checkTiles = (): TILES_ACTION_TYPES => {
+	return {
+		type: CHECK_TILES,
+	}
+}
+
+export const restartGame = (): TILES_ACTION_TYPES => {
+	return {
+		type: RESTART_GAME,
+	}
+}
+
+export const startGame = (): TILES_ACTION_TYPES => {
+	return {
+		type: START_GAME,
+	}
+}
--- src/store/TilesReducer/types.ts
+++ src/store/TilesReducer/types.ts
@@ -1,26 +1,49 @@
 // Types:
 export interface TILE_TYPE {
-	color: string;
+	id: number;
+	
+	frontColor: string;
+	backColor: string;
+	
+	isGuessed: boolean;
 }
 
+// State type:
 export interface TILES_STATE {
-	tiles: Array<TILE_TYPE>,
-	firstTileColor: string,
-	secondTileColor: string
+	tiles: Array<TILE_TYPE>;
+	// Tiles:
+	firstClickedTile: TILE_TYPE | null;
+	secondClickedTile: TILE_TYPE | null;
+	
+	// Size of
+	rowCount: number;
+	columnCount: number;
 }
 
 // Actions types:
 export const HANDLE_CLICK_ON_TILE = 'HANDLE_CLICK_ON_TILE'
-
+export const CHECK_TILES = 'CHECK_TILES'
 export const RESTART_GAME = 'RESTART_GAME'
+export const START_GAME = 'START_GAME'
 
 interface HANDLE_CLICK_ON_TILE_ACTION {
 	type: typeof HANDLE_CLICK_ON_TILE,
-	color: string,
+	tile: TILE_TYPE,
+}
+
+interface CHECK_TILES_ACTION {
+	type: typeof CHECK_TILES,
 }
 
 interface RESTART_GAME_ACTION {
 	type: typeof RESTART_GAME,
 }
 
-export type TILES_ACTIONS = HANDLE_CLICK_ON_TILE_ACTION | RESTART_GAME_ACTION
+interface START_GAME_ACTION {
+	type: typeof START_GAME,
+}
+
+export type TILES_ACTION_TYPES = HANDLE_CLICK_ON_TILE_ACTION |
+	RESTART_GAME_ACTION |
+	START_GAME_ACTION |
+	CHECK_TILES_ACTION
