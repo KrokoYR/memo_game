9bfc1b739f775c238fc5f69cfd09ec48f143061a
Lopson <balzhinimaev-2011@mail.ru>
2020-05-10 21:18:15 +0900

    Added Sidebar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import {Tiles} from './components/Tiles';
+import {Tiles} from './components/Tiles/Tiles';
+import {Sidebar} from "./components/Sidebar/Sidebar";
 
 function App() {
 	return (
 		<div className="App">
 			<Tiles/>
+			<Sidebar/>
 		</div>
 	);
 }
