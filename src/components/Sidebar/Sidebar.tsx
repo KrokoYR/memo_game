b9f8646f138405f77c4f1191c3b2772f635759ea
Lopson <balzhinimaev-2011@mail.ru>
2020-05-11 10:24:55 +0900

    Added sidebar to app, to handle start game and counter of rounds

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,32 +1,63 @@
 import React, {FC} from 'react';
-import {connect} from "react-redux";
-import {Dispatch} from "redux";
+import styles from './Sidebar.module.css'
 
 // Actions for mapDispatchToProps:
-import {AppActions} from "../../store";
+import {AppActions, AppState} from "../../store";
 import {startGame} from "../../store/TilesReducer/actions";
+import {Dispatch} from "redux";
+
+import {connect} from "react-redux";
 
 interface SidebarProps {
+	roundCounter: number;
+	gameIsStarted: boolean;
+	gameIsFinished: boolean;
+	
 	startGame: () => void;
 }
 
-const DumbComponent: FC<SidebarProps> = ({startGame}) => {
+const DumbComponent: FC<SidebarProps> = ({
+	                                         roundCounter,
+	                                         gameIsStarted,
+	                                         gameIsFinished,
+	                                         startGame
+                                         }) => {
 	
 	const handleClickStartButton = () => {
 		startGame();
 	}
 	
+	const element = () => {
+		if (!gameIsStarted && !gameIsFinished) {
+			return 'Click on button to start!'
+		} else if(gameIsStarted && !gameIsFinished){
+			return 'Game is started!'
+		} else {
+			return 'Game is finished'
+		}
+	}
+	
 	return (
-		<div>
-			<button onClick={handleClickStartButton}>Start game</button>
+		<div className={styles.sidebar}>
+			<div><p>{element()}</p></div>
+			<button className={styles.sidebar__start} onClick={handleClickStartButton}>START GAME</button>
+			<button className={styles.sidebar__counter} disabled>ROUNDS: {roundCounter}</button>
 		</div>
 	)
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<AppActions>) => {
+const mapStateToProps = (state: AppState) => {
 	return {
-		startGame: () => dispatch(startGame())
+		roundCounter: state.TilesReducer.roundCounter,
+		gameIsStarted: state.TilesReducer.gameIsStarted,
+		gameIsFinished: state.TilesReducer.gameIsFinished,
 	}
 }
 
-export const Sidebar = connect(null, mapDispatchToProps)(DumbComponent);
+const mapDispatchToProps = (dispatch: Dispatch<AppActions>) => {
+	return {
+		startGame: () => dispatch(startGame()),
+	}
+}
+
+export const Sidebar = connect(mapStateToProps, mapDispatchToProps)(DumbComponent);
