dd3cd888d6219d21cf6cf992284ce6ccf7891536
Lopson <balzhinimaev-2011@mail.ru>
2020-05-15 21:21:55 +0900

    Updates

---
+++ precache-manifest.0b0c864adf101cfb172ca7b4cc23da04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792e53e49de6e180dd3593e25f8526af",
+    "url": "/memo_game_io/index.html"
+  },
+  {
+    "revision": "0aaa1500e86a95a865fe",
+    "url": "/memo_game_io/static/css/main.218e29ae.chunk.css"
+  },
+  {
+    "revision": "b427fe46fba0785d527b",
+    "url": "/memo_game_io/static/js/2.0f89fcf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memo_game_io/static/js/2.0f89fcf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aaa1500e86a95a865fe",
+    "url": "/memo_game_io/static/js/main.da9ddf17.chunk.js"
+  },
+  {
+    "revision": "0c0f00dd0631e384d963",
+    "url": "/memo_game_io/static/js/runtime-main.ca5ae9a4.js"
+  }
+]);
